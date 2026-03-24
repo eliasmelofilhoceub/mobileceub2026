77824a53e6480f4ccfe1e040389830fc8e647148
eliasmelofilhoceub <elias.nascimento@ceub.edu.br>
2026-03-24 14:03:46 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,92 +1,60 @@
-import React, {Component} from "react";
-import {View, Text, Image, Button, StyleSheet} from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TextInput, Button} from 'react-native';
 
-const Card = ({title, description}) => {
-  return(
-    <View style={sytles.card}>
-      <Text style={sytles.title}>{title}</Text>
-      <Text style={sytles.description}>{description}</Text>
-    </View>
-  );
-};
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        nome: '',
+        input: ''
+      };
 
-class App extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            nome: ""
-        };
-        this.entrar = this.entrar.bind(this);
-    }
+      this.entradaNome = this.entradaNome.bind(this);
+  }
 
-    entrar(nome) {
-        this.setState({
-            nome: nome
-        })
-    }
+  entradaNome() {
+      if(this.state.input === '') {
+          alert('Digite seu nome: ');
+          return;
+      }
+
+      this.setState({nome: 'Seja bem vindo a aplicação: ' + this.state.input});
+  }
+
   render() {
     return(
-      <View style={sytles.container}>
-        <Text style={{color: '#4B0082', fontSize: 25, margin: 10}}>
-          Aplicação Inicial
-          </Text>
-        <Text>
-          Primeiro Projeto - Padrão</Text>
-          <Button title="entrar" onPress={() => this.entrar('Seja bem vindo a aplicação')}
+      <View style={styles.contanier}>
+          <TextInput
+          style={styles.input}
+          placeholder="Digite seu nome: "
+          underlineColorAndroid="transparent"
+          onChangeText={(texto) => this.setState({input: texto})}
           />
-          <Text style={{fontSize: 12, color: 'green', textAlign: 'center'}}>
-            {this.state.nome}
-          </Text>
-          <Image
-          source={{uri: 'https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg'}}
-          style={{width: 300, height: 300}}
-          />
+      <Button title="Login" onPress={this.entradaNome}/>
 
-        <Card title='Título' description='Este é um exemplo de Card'
-        />
-
-          <Jobs
-          largura={200} altura={200}
-          />
+          <Text style={styles.texto}>{this.state.nome}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    contanier: {
+      flex: 1,
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: 'red',
+      margin: 12,
+      fontSize: 20,
+      padding: 10,
+    },
+    texto: {
+      fontSize: 18,
+      textAlign: 'center',
+    },
+}); 
+
 export default App;
 
-const sytles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "flex-start",
-    backgroundColor: 'yellow',
-  },
-  title: {
-    fontSize: 16,
-    margin: 8,
-  },
-  description: {
-    fontSize: 11,
-    color: 'green',
-  },
-  card: {
-    width: 300,
-    height: 200,
-    padding: 20,
-    borderRadius: 10,
-  },
-})
 
-class Jobs extends Component {
-    render(){
-        let img = 'https://ciclovivo.com.br/wp-content/uploads/2016/08/foto_natureza_2.jpeg';
-        return(
-            <View>
-                <Image
-                source={{uri: img}}
-                style={{width: this.props.largura, height: this.props.altura}}
-                />
-                <Text style={{textAlign: 'center'}}>Imagem de Natureza</Text>
-            </View>
-        );
-    }
-}
