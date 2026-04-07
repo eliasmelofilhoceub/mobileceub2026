36532f0c39f068cff34d924044e963af73dd55c9
eliasmelofilhoceub <elias.nascimento@ceub.edu.br>
2026-04-07 14:17:13 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,92 +1,48 @@
 import React, {Component} from "react";
-import {View, Text, Image, Button, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button, Modal} from 'react-native'
+import Entrar from "./src/Entrar";
 
-const Card = ({title, description}) => {
-  return(
-    <View style={sytles.card}>
-      <Text style={sytles.title}>{title}</Text>
-      <Text style={sytles.description}>{description}</Text>
-    </View>
-  );
-};
-
-class App extends Component{
+export default class App extends Component{
     constructor(props) {
-        super(props);
-        this.state = {
-            nome: ""
-        };
-        this.entrar = this.entrar.bind(this);
+      super(props);
+      this.state = {
+        modalVisible: false
+      };
+
+      this.entrar = this.entrar.bind(this);
+      this.sair = this.sair.bind(this);
+
     }
 
-    entrar(nome) {
-        this.setState({
-            nome: nome
-        })
+    entrar() {
+      this.setState({modalVisible:true});
     }
-  render() {
-    return(
-      <View style={sytles.container}>
-        <Text style={{color: '#4B0082', fontSize: 25, margin: 10}}>
-          Aplicação Inicial
-          </Text>
-        <Text>
-          Primeiro Projeto - Padrão</Text>
-          <Button title="entrar" onPress={() => this.entrar('Seja bem vindo a aplicação')}
-          />
-          <Text style={{fontSize: 12, color: 'green', textAlign: 'center'}}>
-            {this.state.nome}
-          </Text>
-          <Image
-          source={{uri: 'https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg'}}
-          style={{width: 300, height: 300}}
-          />
+    sair(visible) {
+        this.setState({modalVisible: visible});
+    }
 
-        <Card title='Título' description='Este é um exemplo de Card'
-        />
-
-          <Jobs
-          largura={200} altura={200}
-          />
-      </View>
-    );
+    render() {
+      return(
+          <View style={sytles.container}>
+            <Button title="Entrar" onPress={this.entrar}/>
+            <Modal transparent={true} animationType="slide" visible={this.state.modalVisible}>
+            <View style={{margin: 15, flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+              <Entrar fechar={() => this.sair(false)}/>
+            </View>
+            </Modal>
+          </View>
+      );
+    }
   }
-}
-export default App;
-
 const sytles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "flex-start",
-    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DDD',
   },
-  title: {
-    fontSize: 16,
-    margin: 8,
-  },
-  description: {
-    fontSize: 11,
-    color: 'green',
-  },
-  card: {
-    width: 300,
-    height: 200,
-    padding: 20,
-    borderRadius: 10,
-  },
-})
 
-class Jobs extends Component {
-    render(){
-        let img = 'https://ciclovivo.com.br/wp-content/uploads/2016/08/foto_natureza_2.jpeg';
-        return(
-            <View>
-                <Image
-                source={{uri: img}}
-                style={{width: this.props.largura, height: this.props.altura}}
-                />
-                <Text style={{textAlign: 'center'}}>Imagem de Natureza</Text>
-            </View>
-        );
-    }
-}
+});
+
+
+
